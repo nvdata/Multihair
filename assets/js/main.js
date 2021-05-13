ea5078205ec0706042bf73a6eacef8aea205c47e
adr <senhoraroldo@gmail.com>
2021-05-13 16:53:30 -0300

    pull9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // import vemBootstrap from './modules/botstrapimport.js';
 // import * as animacao from './modules/animation.js';
 // import * as carouselClick from './modules/carousel-change.js';
-// import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
-// import * as config from './modules/swiper__props.js'; //Configs do Swiper
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+import * as config from './modules/swiper__props.js'; //Configs do Swiper
 import Dom from './modules/constructors.js'; //selecionar elementos
 import menu from './modules/menu.js'; //js do menu
 // import zoomFont from './modules/zoomFont.js'; //js do menu
@@ -24,10 +24,12 @@ const pageContato = new Dom().el("#page__contato");
 
 if (pageHome) { // ★ HOME 
     new Dom().bodyClass("body__home")
+    new Swiper(".depo__carousel", config.propsDepo)
 
-}
+    if (mediaQuebra)
+        new Swiper(".produtos__container", config.props)
+    new Swiper(".produtos__container-2", config.props)
 
-if (pageContato) { // ★ Contato
+} else if (pageContato) { // ★ Contato
     new Dom().bodyClass("body__contato");
 }
-
